f5808260a2cedcba158da7adf5b5a2cbfa56a0f5
André <33901599+marchildon@users.noreply.github.com>
2023-11-04 12:31:02 -0400

    Cleaned up the plotting functions

--- gpgradpy/plt/plt_cond.py
+++ gpgradpy/plt/plt_cond.py
@@ -20,10 +20,10 @@ n_eval      = 10
 cond_max    = 1e10 # Maximum condition number
 
 # Plotting options
-n_gamma     = 200
+n_gamma     = 10
 range_gamma = np.array([1e-4, 1e8])
 
-save_fig    = True
+save_fig    = False
 folder_all  = path.join('figures', '2D_cond')
 
 label_fs    = 12
@@ -120,16 +120,93 @@ dist_x_eval = distance.cdist(x_eval, x_eval, 'euclidean')
 dist_min    = np.nanmin(dist_x_eval + np.diag(np.full(n_eval, np.nan)))
 print(f'dim = {dim}, n_eval = {n_eval}, nugget = {nugget:.2e}, dist_min = {dist_min:.2e}')
 
-def calc_n_plot(use_grad, kernel_type, wellcond_mtd, 
+range_theta = GP.gamma2theta(range_gamma)
+theta_vec   = np.logspace(np.log10(range_theta[0]), np.log10(range_theta[1]), n_gamma) 
+gamma_vec   = GP.theta2gamma(theta_vec)
+
+Xmat_para, Ymat_para  = np.meshgrid(gamma_vec, gamma_vec)
+
+def setup_plot():
+
+    fig, ax = plt.subplots(figsize=(4, 4))
+    
+    ax.set_xscale('log')
+    ax.set_yscale('log')
+    
+    if dim != 2:
+        raise Exception('Not considered')
+    
+    ax.set_xlabel(r'$\gamma_1$', size=label_fs)
+    ax.set_ylabel(r'$\gamma_2$', size=label_fs, rotation=0)
+    
+    ax.grid(True)
+    ax.tick_params(axis='both', which='major', labelsize=tick_fs)
+    ax.set_aspect('equal', adjustable='box')
+    
+    ax.set_xticks(para_tick_loc)
+    ax.set_yticks(para_tick_loc)
+        
+    return fig, ax
+
+def calc_cond_lkd(use_grad, kernel_type, wellcond_mtd, 
                 use_const_eta   = False, 
                 std_fval_scalar = 0,    std_grad_scalar = 0):
     
-    std_fval_vec  = std_fval_scalar * np.ones(obj_eval.shape)
-    std_grad_vec  = std_grad_scalar * np.ones(grad_eval.shape)
+    std_fval_vec = std_fval_scalar * np.ones(obj_eval.shape)
+    std_grad_vec = std_grad_scalar * np.ones(grad_eval.shape)
     
     print(f'use_grad = {use_grad}, kernel_type = {kernel_type}, wellcond_mtd = {wellcond_mtd}')
 
     ''' Create the GP and evaluate the surrogate'''
+    
+    GP = GaussianProcess(dim, use_grad, kernel_type, wellcond_mtd)
+    GP.use_const_eta = use_const_eta
+    
+    if use_grad:
+        GP.set_data(x_eval, obj_eval, std_fval_vec, grad_eval, std_grad_vec)
+    else:
+        GP.set_data(x_eval, obj_eval, std_fval_vec)
+        
+    GP._etaK      = nugget
+    GP._eta_Kgrad = nugget
+    GP.cond_max   = cond_max
+    
+    hp_kernel   = GP.hp_kernel_default
+    
+    lkd_all     = np.full((n_gamma, n_gamma), np.nan)
+    condK_all   = np.full((n_gamma, n_gamma), np.nan)
+    
+    for i in range(n_gamma):
+        for j in range(n_gamma):
+            theta    = np.array([theta_vec[i], theta_vec[j]])
+            hp_vals  = GP.make_hp_class(varK = varK, theta = theta, kernel = hp_kernel)
+            lkd_info = GP.calc_lkd_all(hp_vals, calc_lkd = True, calc_cond = True, calc_grad = False)[0]
+            
+            lkd_all[i,j]   = lkd_info.ln_lkd
+            condK_all[i,j] = lkd_info.cond
+            
+    ''' Identify max likelihood ''' 
+    
+    if np.sum(np.isnan(lkd_all)) == n_gamma**2:
+        loc_max_lkd  = None
+    else:
+        max_each_row = np.nanmax(lkd_all, axis=1)
+        idx_max_col  = np.nanargmax(max_each_row)
+        idx_max_row  = np.nanargmax(lkd_all[idx_max_col, :])
+        
+        loc_max_lkd  = np.array([gamma_vec[idx_max_row], gamma_vec[idx_max_col]])
+        
+    return condK_all, lkd_all, loc_max_lkd
+
+def calc_scl_data(data):
+    
+    data_min = np.nanmin(data)
+    data_max = np.nanmax(data)
+    data_scl = (data - data_min) / (data_max - data_min)    
+    
+    return data_scl
+
+def set_base_fig_name(kernel_type, std_fval_scalar, std_grad_scalar):
     
     if std_fval_scalar > 0:
         str_noise = f'_StdObj_{std_fval_scalar:.1e}'.replace('.', 'p')
@@ -147,119 +224,70 @@ def calc_n_plot(use_grad, kernel_type, wellcond_mtd,
         base_file_name = f'Kern_{kernel_type}_grad_F_n{n_eval}{str_noise}.png'
         assert wellcond_mtd is None, 'If use_grad is False, then wellcond_mtd must be None'
         
-    GP = GaussianProcess(dim, use_grad, kernel_type, wellcond_mtd)
-    GP.use_const_eta = use_const_eta
+    return base_file_name
+
+def plot_cond_number(fig, ax, log10_condK_all, loc_max_lkd = None, base_file_name = None):
     
-    if use_grad:
-        GP.set_data(x_eval, obj_eval, std_fval_vec, grad_eval, std_grad_vec)
-    else:
-        GP.set_data(x_eval, obj_eval, std_fval_vec)
-        
-    GP._etaK      = nugget
-    GP._eta_Kgrad = nugget
-    GP.cond_max   = cond_max
+    # Xmat_para, Ymat_para  = np.meshgrid(gamma_vec, gamma_vec)
     
-    hp_kernel   = GP.hp_kernel_default
-    
-    range_theta = GP.gamma2theta(range_gamma)
-    theta_vec   = np.logspace(np.log10(range_theta[0]), np.log10(range_theta[1]), n_gamma) 
-    gamma_vec   = GP.theta2gamma(theta_vec)
-    
-    lkd_all     = np.full((n_gamma, n_gamma), np.nan)
-    condK_all   = np.full((n_gamma, n_gamma), np.nan)
-    
-    for i in range(n_gamma):
-        for j in range(n_gamma):
-            theta    = np.array([theta_vec[i], theta_vec[j]])
-            hp_vals  = GP.make_hp_class(varK = varK, theta = theta, kernel = hp_kernel)
-            lkd_info = GP.calc_lkd_all(hp_vals, calc_lkd = True, calc_cond = True, calc_grad = False)[0]
-            
-            lkd_all[i,j]   = lkd_info.ln_lkd
-            condK_all[i,j] = lkd_info.cond
-            
-    log10_condK_all = np.log10(condK_all)
-    # etaK_scl_all    = GP._etaK / etaK_all
-    print(f'wellcond_mtd = {wellcond_mtd}, cond: min = {np.min(log10_condK_all):.2}, max = {np.max(log10_condK_all):.2}')
-            
-    min_lkd = np.nanmin(lkd_all)
-    max_lkd = np.nanmax(lkd_all)
-    scl_lkd_all = (lkd_all - min_lkd) / (max_lkd - min_lkd)    
-    
-    ''' Identify max likelihood ''' 
-    
-    if np.sum(np.isnan(scl_lkd_all)) == n_gamma**2:
-        loc_max_lkd  = None
-    else:
-        max_each_row = np.nanmax(scl_lkd_all, axis=1)
-        idx_max_col  = np.nanargmax(max_each_row)
-        idx_max_row  = np.nanargmax(scl_lkd_all[idx_max_col, :])
-        
-        loc_max_lkd  = np.array([gamma_vec[idx_max_row], gamma_vec[idx_max_col]])
-    
-    def setup_plot():
-    
-        fig, ax = plt.subplots(figsize=(4, 4))
-        
-        ax.set_xscale('log')
-        ax.set_yscale('log')
-        
-        if dim != 2:
-            raise Exception('Not considered')
-        
-        ax.set_xlabel(r'$\gamma_1$', size=label_fs)
-        ax.set_ylabel(r'$\gamma_2$', size=label_fs, rotation=0)
-        
-        ax.grid(True)
-        ax.tick_params(axis='both', which='major', labelsize=tick_fs)
-        ax.set_aspect('equal', adjustable='box')
-        
-        ax.set_xticks(para_tick_loc)
-        ax.set_yticks(para_tick_loc)
-            
-        return fig, ax
-        
-    ''' Plot the condition number with eta '''
-    
-    Xmat_para, Ymat_para  = np.meshgrid(gamma_vec, gamma_vec)
-    
-    fig0, ax0 = setup_plot()
-    
-    cs = ax0.contourf(Xmat_para, Ymat_para, log10_condK_all, levels=lvl_cond, cmap=cmap_cond, extend='max')
+    cs = ax.contourf(Xmat_para, Ymat_para, log10_condK_all, levels=lvl_cond, cmap=cmap_cond, extend='max')
     
     if loc_max_lkd is not None:
-        ax0.plot(loc_max_lkd[0], loc_max_lkd[1], 'k*', markersize = markersize)
+        ax.plot(loc_max_lkd[0], loc_max_lkd[1], 'k*', markersize = markersize)
             
-    fig0.colorbar(cs, pad = 0.08, shrink = plt_shrink, aspect = plt_aspect)
+    fig.colorbar(cs, pad = 0.08, shrink = plt_shrink, aspect = plt_aspect)
     
-    fig0.tight_layout()
+    fig.tight_layout()
     plt.show() 
     
-    if save_fig:
+    if save_fig and (base_file_name is not None):
         file_name = 'Cond_' + base_file_name
         full_path = path.join(folder_all, file_name)
-        fig0.savefig(full_path, dpi = 800, format='png', bbox_inches='tight')
+        fig.savefig(full_path, dpi = 800, format='png', bbox_inches='tight')
+
+def plot_lkd(fig, ax, scl_lkd_all, log10_condK_all, loc_max_lkd = None, base_file_name = None):
     
-    ''' Plot the likelihood number '''
-    
-    fig2, ax2 = setup_plot()
-    
-    cs = ax2.contourf(Xmat_para, Ymat_para, scl_lkd_all, levels=lvl_lkd, cmap=cmap_lkd)
+    cs = ax.contourf(Xmat_para, Ymat_para, scl_lkd_all, levels=lvl_lkd, cmap=cmap_lkd)
     
     if loc_max_lkd is not None:
-        ax2.plot(loc_max_lkd[0], loc_max_lkd[1], 'k*', markersize = markersize)
+        ax.plot(loc_max_lkd[0], loc_max_lkd[1], 'k*', markersize = markersize)
         
-    if np.max(log10_condK_all) > np.log10(cond_max):
-        ax2.contour(Xmat_para, Ymat_para, scl_lkd_all, levels=[np.log10(cond_max)])
+    if log10_condK_all is not None:
+        if np.max(log10_condK_all) > np.log10(cond_max):
+            ax.contour(Xmat_para, Ymat_para, scl_lkd_all, levels=[np.log10(cond_max)])
     
-    fig2.colorbar(cs, pad = 0.08, shrink = plt_shrink, aspect = plt_aspect)
+    fig.colorbar(cs, pad = 0.08, shrink = plt_shrink, aspect = plt_aspect)
     
-    fig2.tight_layout()
+    fig.tight_layout()
     plt.show() 
     
-    if save_fig:
+    if save_fig and (base_file_name is not None):
         file_name = 'Lkd_' + base_file_name
         full_path = path.join(folder_all, file_name)
-        fig2.savefig(full_path, dpi = 800, format='png', bbox_inches='tight')
+        fig.savefig(full_path, dpi = 800, format='png', bbox_inches='tight')
+
+def calc_n_plot(use_grad, kernel_type, wellcond_mtd, 
+                use_const_eta   = False, 
+                std_fval_scalar = 0,    std_grad_scalar = 0):
+    
+    ''' Calculations '''
+    
+    base_file_name = set_base_fig_name(kernel_type, std_fval_scalar, std_grad_scalar)
+    
+    condK_all, lkd_all, loc_max_lkd \
+        = calc_cond_lkd(use_grad, kernel_type, wellcond_mtd, use_const_eta, 
+                        std_fval_scalar, std_grad_scalar)
+        
+    log10_condK_all = np.log10(condK_all)
+    scl_lkd_all     = calc_scl_data(lkd_all)
+    
+    ''' Plot '''
+    
+    fig0, ax0 = setup_plot()
+    plot_cond_number(fig0, ax0, log10_condK_all, loc_max_lkd, base_file_name)
+    
+    fig1, ax1 = setup_plot()
+    plot_lkd(fig1, ax1, scl_lkd_all, log10_condK_all, loc_max_lkd, base_file_name)
         
 ''' Grad-free, noise-free '''
 
@@ -313,17 +341,17 @@ if False:
     
 ''' Non-Gaussian kernels with gradients '''
 
-# # Baseline method with constant nuggget
-# if False:
-#     use_grad         = True
-#     kernel_type_vec  = ['Ma5f2', 'RatQu']
-#     wellcond_mtd     = None
-#     use_const_eta    = False
-#     std_fval_scalar  = 0
-#     std_grad_scalar  = 0
+# Baseline method with constant nuggget
+if True:
+    use_grad         = True
+    kernel_type      = 'SqExp'
+    wellcond_mtd     = None
+    use_const_eta    = False
+    std_fval_scalar  = 0
+    std_grad_scalar  = 0
     
-#     calc_n_plot(use_grad, kernel_type, wellcond_mtd, use_const_eta, 
-#                 std_fval_scalar, std_grad_scalar)
+    calc_n_plot(use_grad, kernel_type, wellcond_mtd, use_const_eta, 
+                std_fval_scalar, std_grad_scalar)
 
 # Precon method with variable nugget
 if False:
@@ -341,7 +369,7 @@ if False:
 
 ''' All kernels with grad, noisy-case '''
 
-if True:
+if False:
     use_grad         = True
     kernel_type_vec  = ['SqExp', 'Ma5f2', 'RatQu']
     wellcond_mtd     = 'precon'
@@ -352,3 +380,6 @@ if True:
     for kernel_type in kernel_type_vec:
         calc_n_plot(use_grad, kernel_type, wellcond_mtd, use_const_eta, 
                     std_fval_scalar, std_grad_scalar)
+
+
+
